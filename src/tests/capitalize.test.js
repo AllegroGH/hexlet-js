c441fe47307907d4616cd96383b50a8b31ceeaae
Allegro <alex.makovka@gmail.com>
2023-02-07 22:00:23 +0300

    add node Asserts test

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
+import { strict as assert } from 'node:assert';
 import capitalize from '../capitalize.js';
 
-if (capitalize('hello') !== 'Hello') throw new Error('Функция работает неверно!');
-if (capitalize('') !== '') throw new Error('Функция работает неверно!');
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 console.log('Все тесты пройдены!');

4f60089d8b16d792b477a61e75ea82f39775f8bf
Allegro <alex.makovka@gmail.com>
2023-02-07 20:50:04 +0300

    add capitalize.js & test.js

---
+++ src/tests/capitalize.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import capitalize from '../capitalize.js';
+
+if (capitalize('hello') !== 'Hello') throw new Error('Функция работает неверно!');
+if (capitalize('') !== '') throw new Error('Функция работает неверно!');
+console.log('Все тесты пройдены!');
